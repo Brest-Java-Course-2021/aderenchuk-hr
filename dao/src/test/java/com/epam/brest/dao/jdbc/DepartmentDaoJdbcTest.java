b5333315b117ff419a73e6bf5d4b51f13e8aba7b
Artsiom Dzeranchuk <derenchuk2001@mail.ru>
2021-03-02 21:33:10 +0300

    add logging

--- dao/src/test/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbcTest.java
+++ dao/src/test/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbcTest.java
@@ -6,6 +6,8 @@ import com.epam.brest.model.Department;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,6 +18,8 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
     public class DepartmentDaoJdbcTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentDaoJdbcTest.class);
 
         @Autowired
         private DepartmentDao departmentDao;
@@ -44,8 +48,6 @@ import java.util.List;
     @Test (expected = EmptyResultDataAccessException.class)
     public void findByIdExceptionalTest() {
             departmentDao.findById(999).get();
-            //Assert.assertTrue(expDepartment, );
-
     }
 
     @Test
@@ -61,4 +63,12 @@ import java.util.List;
         Assert.assertEquals(departments.size() + 1, realDepartments.size());
     }
 
+    @Test
+    public void testLogger() {
+        LOGGER.trace("hello trace");
+        LOGGER.debug("hello debug");
+        LOGGER.info("hello info");
+        LOGGER.warn("hello warn");
+        LOGGER.error("hello error");
+    }
 }
