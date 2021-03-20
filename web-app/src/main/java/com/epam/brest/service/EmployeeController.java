f1aa9a2b1a0e47781a608d0ffc77f41cd44adc89
Artsiom Dzeranchuk <derenchuk2001@mail.ru>
2021-03-20 11:59:54 +0300

    add EmployeeController

---
+++ web-app/src/main/java/com/epam/brest/service/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.epam.brest.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping(value = "/employees")
+    public final String employees(Model model) {
+        return "employees";
+    }
+
+    @GetMapping(value = "/employee/{id}")
+    public final String gotoEditEmployeePage(@PathVariable Integer id, Model model) {
+        return "employee";
+    }
+
+    @GetMapping(value = "/employee/add")
+    public final String gotoAddEmployeePage(Model model) {
+        return "employee";
+    }
+}
